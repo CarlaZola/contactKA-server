36fa6b134294d4e046a0e58761cab244229b9932
carla <carla.zollaa@gmail.com>
2023-07-25 11:02:27 -0300

    feat: tests for create new user

--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,38 +1,7 @@
-export default {
-    userComplete: {
-      full_name: 'Pedro',
-      phone: "48999998774",
-      email: 'pedro@kenzie.com.br',
-      password: '1234',
-      admin: true,
-    },
-    userWithoutAdmin: {
-      full_name: 'Alex',
-      phone: "48999998774",
-      email: 'alex@kenzie.com.br',
-      password: '1234',
-    },
-    userUnique: {
-      full_name: 'Felipe',
-      phone: "48999952356",
-      email: 'felipe@kenzie.com.br',
-      password: '1234',
-    },
-    userInvalidBody: {
-      full_name: 1234,
-      password: '1234',
-      email: [],
-    },
-    userInvalidBody2: {
-      full_name: 'Alex',
-      email: 'mail',
-      password: 123456,
-    },
-    userInvalidBody3: {
-        full_name: 'Flanders',
-        email: 'mailinvalidin',
-        password: 123456,
-        phone: "um fone nada a ver com mais de 11 chares",
-      },
-  };
-  
+import createUserRouteMock from "./users/createUser.route.mock";
+import errors from "./errors.mock";
+
+export {
+  createUserRouteMock,
+  errors
+}
---
+++ src/__tests__/mocks/users/createUser.route.mock.ts
@@ -0,0 +1,47 @@
+export default {
+    userComplete: {
+        full_name: 'Pedro Hasler',
+        phone: "(47) 99602-0874",
+        email: 'pedro@kenzie.com.br',
+        password: '1234',
+        nickname: "Pedroca"
+    },
+    userWithoutNickname: {
+        full_name: 'Alex Conder',
+        phone: "(21) 99102-7895",
+        email: 'alex@kenzie.com.br',
+        password: '1234',
+    },
+    userInvalidPhone: {
+        full_name: 'Flanders',
+        email: 'flanders@gmail.com',
+        password: '123456',
+        phone: "45998123540",
+    },
+    userInvalidEmail: {
+        full_name: 'Gustavo Mota',
+        email: 'mail',
+        phone: "(18) 99245-4587",
+        password: '1234',
+    },
+    userUniqueEmail: {
+        full_name: 'Felipe Silva',
+        phone: "(16) 99245-4587",
+        email: 'felipe@kenzie.com.br',
+        password: '1234',
+        },
+    userUniqueName: {
+        full_name: "Andyara Oss",
+        password: '1234',
+        phone: "(47) 99245-4587",
+        email: "andy@kenzie.com.br",
+    },
+    userUniqueName2: {
+        full_name: "Andyara Oss",
+        password: '1234',
+        phone: "(47) 99245-4587",
+        email: "andy2@kenzie.com.br",
+    },
+
+};
+  
